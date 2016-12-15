3310746f875aecdcd615cfdcbc0c0fa8a09c722e
josebummer <jantonioruiz@correo.ugr.es>
2016-12-15 21:33:44 +0100

    java completo

--- Napakalaki/src/napakalaki/CultistPlayer.java
+++ Napakalaki/src/napakalaki/CultistPlayer.java
@@ -20,25 +20,25 @@ public class CultistPlayer extends Player{
         myCultistCard = c;
         totalCultistPlayer++;
     }
-    
+    @Override
     protected int getCombatLevel(){
-        return this.getCombatLevel() + this.getCombatLevel()*20/100 + myCultistCard.getGainedLevels()*totalCultistPlayer;
+        return super.getCombatLevel() + super.getCombatLevel()*20/100 + myCultistCard.getGainedLevels()*totalCultistPlayer;
     }
-    
+    @Override
     protected int getOponentLevel(Monster m){
         return m.getCombatLevelAgainstCultistPlayer();
     }
-    
+    @Override
     protected boolean shouldConvert(){
         return false;
     }
-    
+
     private Treasure giveMeATreasure(){
         Random  rnd = new Random();
         int number = (int) (rnd.nextDouble() * super.getVisibleTreasures().size());
         return super.getVisibleTreasures().get(number);
     }
-    
+   
     private boolean canYouGiveMeATreasure(){
         return !super.getVisibleTreasures().isEmpty();
     }
